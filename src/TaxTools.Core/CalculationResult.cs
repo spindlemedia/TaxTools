8815e8aa5743587c9337d9c4191f06900fd93dc5
Mark Gould <markgould@users.noreply.github.com>
2023-07-20 13:46:04 -0500

    Change from SB1 to SB2 logic (#14)

--- src/TaxTools.Core/CalculationResult.cs
+++ src/TaxTools.Core/CalculationResult.cs
@@ -13,10 +13,10 @@
         public decimal AdditionalImprovement { get; set; }
         public int TaxableValue { get; set; }
         public decimal SB12Reduction { get; set; }
-        public decimal? SB1Reduction { get; set; }
-        public decimal RunningTotal => Math.Max(StartingAmount + AdditionalImprovement - SB12Reduction - (SB1Reduction ?? 0), 0);
+        public decimal? SB2Reduction { get; set; }
+        public decimal RunningTotal => Math.Max(StartingAmount + AdditionalImprovement - SB12Reduction - (SB2Reduction ?? 0), 0);
 
         public string SB12CalculationText { get; set; }
-        public string SB1CalculationText { get; set; }
+        public string SB2CalculationText { get; set; }
     }
 }
