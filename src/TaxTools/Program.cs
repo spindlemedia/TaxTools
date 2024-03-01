f60a8e6be0935327e14e42517f1656295fc4d710
Mark Gould <markgould@users.noreply.github.com>
2024-03-01 13:10:07 -0600

    Add Radzen components and allow for multiple calculator tabs (#26)

--- src/TaxTools/Program.cs
+++ src/TaxTools/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TaxTools;
 using System.Globalization;
+using Radzen;
 
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
@@ -11,5 +12,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddRadzenComponents();
 await builder.Build().RunAsync();
